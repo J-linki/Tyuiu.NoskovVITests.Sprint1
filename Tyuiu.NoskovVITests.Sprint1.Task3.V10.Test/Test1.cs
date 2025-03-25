afb5d0e27f0d6116d3a016e9db42b7817f0e52b9
J-linki <vital.noskov.03@gmail.com>
2025-03-25 17:23:16 +0500

    Add project files.

---
+++ Tyuiu.NoskovVITests.Sprint1.Task3.V10.Test/Test1.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.NoskovVITests.Sprint1.Task3.V10.Lib;
+
+namespace Tyuiu.NoskovVITests.Sprint1.Task3.V10.Test
+{
+	[TestClass]
+	public sealed class Test1
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+			DataService ds = new DataService();
+			Assert.AreEqual(ds.NumberToMoney(305), "30.5 руб. - это 30 руб. 50 коп.");
+		}
+	}
+}
---
+++ Tyuiu.NoskovVITests.Sprint1.Task3.V10/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.NoskovVITests.Sprint1.Task3.V10.Lib;
+
+namespace Tyuiu.NoskovVITests.Sprint1.Task3.V10
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			DataService ds = new DataService();
+			Console.WriteLine(ds.NumberToMoney(305));
+		}
+	}
+}
